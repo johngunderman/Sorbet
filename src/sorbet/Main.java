2940005276abf9252086d3ab76d210b0fc17297b
Jacob Snyder <jrs213@case.edu>
2011-10-29 19:03:25 -0400

    Add launchVirtualMachine method

--- src/sorbet/Main.java
+++ src/sorbet/Main.java
@@ -1,7 +1,7 @@
 package sorbet;
 
+import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
 import java.util.Map;
 
 import com.sun.jdi.Bootstrap;
@@ -10,45 +10,58 @@ import com.sun.jdi.VirtualMachineManager;
 import com.sun.jdi.connect.Connector;
 import com.sun.jdi.connect.IllegalConnectorArgumentsException;
 import com.sun.jdi.connect.LaunchingConnector;
+import com.sun.jdi.connect.VMStartException;
 
 public class Main {	
 	public static void main(String args[]) {		
+		VirtualMachine vm = launchVirtualMachine("client.Main");
+		
+		vm.resume();
+			
+		InputStream errorStream = vm.process().getErrorStream();
+		InputStream outputStream = vm.process().getInputStream();
+		
+		while (true) {
+			int error = errorStream.read();
+			if (error != -1) {
+				System.out.print((char)error);
+			}
+			
+			int output = outputStream.read();
+			if (output != -1) {
+				System.out.print((char)output);
+			}
+		}
+	}	
+	
+	private static VirtualMachine launchVirtualMachine(String mainArg) {
 		VirtualMachineManager manager = Bootstrap.virtualMachineManager();
 		
 		LaunchingConnector connector = manager.defaultConnector();
 		
 		Map<String, Connector.Argument> arguments = connector.defaultArguments();
 		
-		Connector.Argument mainArgument = arguments.get("main");
-		mainArgument.setValue("client.Main");
+		arguments.get("main").setValue(mainArg);
 		
 		try {
 			VirtualMachine vm = connector.launch(arguments);
 			
-			vm.resume();
+			return vm;
+		}
+		catch (IOException e) {
+            throw new Error("Error: Could not launch target VM: " + e.getMessage());
+        } 
+		catch (IllegalConnectorArgumentsException e) {
+			StringBuffer args = new StringBuffer();
 			
-			InputStream errorStream = vm.process().getErrorStream();
-			InputStream outputStream = vm.process().getInputStream();
+			for (String arg : e.argumentNames()) {
+				args.append(arg);
+			}
 			
-			while (true) {
-				int error = errorStream.read();
-				if (error != -1) {
-					System.out.print((char)error);
-				}
-				
-				int output = outputStream.read();
-				if (output != -1) {
-					System.out.print((char)output);
-				}
-			}
-		}
-		catch(IllegalConnectorArgumentsException e)	{
-			for (String argName : e.argumentNames()) {
-				System.out.println(argName);
-			}
-		}
-		catch(Exception e) {
-			System.out.println(e.toString());
-		}
-	}	
+            throw new Error("Error: Could not launch target VM because of illegal arguments: " + args.toString());
+        } 
+		catch (VMStartException e) {
+            throw new Error("Error: Could not launch target VM: " + e.getMessage());
+        }
+	}
 }
