dff3316a11da89a57113c8def14d45de2877cd4f
John Gunderman <johngunderman@gmail.com>
2011-10-29 20:48:36 -0400

    attempt to implement basic variable watching

--- src/client/Main.java
+++ src/client/Main.java
@@ -1,7 +1,15 @@
 package client;
 
 public class Main {
+	
+	static int foo = 0;
+	
 	public static void main(String[] args) {
+		
+		foo = 3;
+		
+		foo = 5;
+		
 		System.out.println("Client program");
 	}
 }
--- src/sorbet/Main.java
+++ src/sorbet/Main.java
@@ -2,20 +2,50 @@ package sorbet;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.List;
 import java.util.Map;
 
 import com.sun.jdi.Bootstrap;
+import com.sun.jdi.Field;
+import com.sun.jdi.ReferenceType;
 import com.sun.jdi.VirtualMachine;
 import com.sun.jdi.VirtualMachineManager;
 import com.sun.jdi.connect.Connector;
 import com.sun.jdi.connect.IllegalConnectorArgumentsException;
 import com.sun.jdi.connect.LaunchingConnector;
 import com.sun.jdi.connect.VMStartException;
+import com.sun.jdi.event.Event;
+import com.sun.jdi.event.EventQueue;
+import com.sun.jdi.event.EventSet;
+import com.sun.jdi.event.ModificationWatchpointEvent;
+import com.sun.jdi.event.VMDeathEvent;
+import com.sun.jdi.event.VMDisconnectEvent;
+import com.sun.jdi.request.ClassPrepareRequest;
+import com.sun.jdi.request.EventRequestManager;
+import com.sun.jdi.request.ModificationWatchpointRequest;
 import com.sun.xml.internal.bind.v2.util.EditDistance;
 
+
 public class Main {	
+
+	public static final String FIELD_NAME = "foo";
+	public static final String CLASS_NAME = "sMain";
+	
 	public static void main(String args[]) {		
 		VirtualMachine vm = launchVirtualMachine("client.Main");
+	
+		List<ReferenceType> referenceTypes = vm.classesByName(CLASS_NAME);
+		for (ReferenceType refType : referenceTypes) {
+			addFieldWatch(vm, refType);
+		}
+		
+		
+		EventRequestManager erm = vm.eventRequestManager();
+		ClassPrepareRequest classPrepareRequest = erm
+				.createClassPrepareRequest();
+		classPrepareRequest.addClassFilter(CLASS_NAME);
+		classPrepareRequest.setEnabled(true);
+		
 		
 		vm.resume();
 			
@@ -31,12 +61,35 @@ public class Main {
 		errorStreamThread.start();
 		outputStreamThread.start();
 		
-		while (true) {
-			
-		}	
 		
-		//vm.exit(0);
-	}	
+	    EventQueue eventQueue = vm.eventQueue();
+
+		while (true) {
+			EventSet eventSet;
+			try {
+				eventSet = eventQueue.remove();
+				for (Event event : eventSet) {
+					if (event instanceof VMDeathEvent
+							|| event instanceof VMDisconnectEvent) {
+						// exit
+						return;
+					} else if (event instanceof ModificationWatchpointEvent) {
+						// a Test.foo has changed
+						ModificationWatchpointEvent modEvent = (ModificationWatchpointEvent) event;
+						System.out.println("old=" + modEvent.valueCurrent());
+						System.out.println("new=" + modEvent.valueToBe());
+						System.out.println();
+					}
+				}
+				eventSet.resume();
+				
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+		
 	
 	private static VirtualMachine launchVirtualMachine(String mainArg) {
 		VirtualMachineManager manager = Bootstrap.virtualMachineManager();
@@ -67,5 +120,13 @@ public class Main {
 		catch (VMStartException e) {
             throw new Error("Error: Could not launch target VM: " + e.getMessage());
         }
+	}
+	
+	private static void addFieldWatch(VirtualMachine vm, ReferenceType refType) {
+		EventRequestManager erm = vm.eventRequestManager();
+		Field field = refType.fieldByName(FIELD_NAME);
+		ModificationWatchpointRequest modificationWatchpointRequest = erm
+				.createModificationWatchpointRequest(field);
+		modificationWatchpointRequest.setEnabled(true);
 	}
 }
