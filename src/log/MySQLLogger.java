16e8b76a839b02f637dff1b6a603581a3ba24ef6
John Gunderman <johngunderman@gmail.com>
2011-11-01 21:42:32 -0400

    Add emtpy logging classes

---
+++ src/log/MySQLLogger.java
@@ -0,0 +1,11 @@
+package log;
+
+public class MySQLLogger implements Loggable {
+
+	@Override
+	public int log() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
---
+++ src/log/TextLogger.java
@@ -0,0 +1,11 @@
+package log;
+
+public class TextLogger implements Loggable {
+
+	@Override
+	public int log() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
