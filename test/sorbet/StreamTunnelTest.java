58e4ea7a220d8031f8d67a431ae6ce2fd7fa693a
Jacob Snyder <jrs213@case.edu>
2011-11-16 17:20:50 -0500

    Wrote StreamTunnel test method

--- test/sorbet/StreamTunnelTest.java
+++ test/sorbet/StreamTunnelTest.java
@@ -2,6 +2,13 @@ package sorbet;
 
 import static org.junit.Assert.*;
 
+import java.io.BufferedInputStream;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -10,25 +17,33 @@ import org.junit.Test;
 
 public class StreamTunnelTest {
 
-	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
-	}
-
-	@AfterClass
-	public static void tearDownAfterClass() throws Exception {
-	}
-
-	@Before
-	public void setUp() throws Exception {
-	}
-
-	@After
-	public void tearDown() throws Exception {
-	}
-
 	@Test
-	public void testStreamTunnel() {
-		fail("Not yet implemented");
+	public void testStreamTunnel() {		
+		// Create the random data to test with
+		byte[] inputData = new byte[1024];
+		for (int i = 0; i < inputData.length; i++) {
+			inputData[i] = (byte)(Math.random() * Byte.MAX_VALUE);
+		}
+		
+		// Create our input and output streams
+		ByteArrayInputStream in = new ByteArrayInputStream(inputData);
+		ByteArrayOutputStream out = new ByteArrayOutputStream();
+		
+		// Create the StreamTunnel to test
+		StreamTunnel tunnel = new StreamTunnel(in, out);
+		
+		// Wait a bit just to make sure the tunnel thread has time to process
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			// Do nothing
+		}
+		
+		// Get the output data
+		byte[] outputData = out.toByteArray();
+			
+		// Check to make sure they're equal
+		assertArrayEquals(outputData, inputData);
 	}
 
 }
