ddde19a9086d692e6f7564edbfe556a5f141fa2b
John Gunderman <johngunderman@gmail.com>
2011-11-27 21:15:48 -0500

    add final support for logging sqlite. as of yet untested.

--- src/log/SQLiteLogger.java
+++ src/log/SQLiteLogger.java
@@ -38,15 +38,13 @@ public class SQLiteLogger extends Logger {
 			"varsid INTEGER REFERENCES VARS (varsid), " +
 			"linesid INTEGER REFERENCES LINES (linesid)," +
 			"value TEXT, " +
-			"type TEXT," +
 			"PRIMARY KEY (runsid, varvaluesid))";
 
 	private static String createVarDeath = "CREATE TABLE IF NOT EXISTS VARDEATH (" +
 			"runsid INTEGER REFERENCES RUNS (runsid)," +
-			"vardeathid INTEGER," +
 			"varsid INTEGER REFERENCES VARS (varsid)," +
 			"linesid INTEGER REFERENCES LINES (linesid)," +
-			"PRIMARY KEY (runsid, vardeathid))";
+			"PRIMARY KEY (runsid, varsid))";
 
 	private static String createVarUsed = "CREATE TABLE IF NOT EXISTS VARUSED (" +
 			"runsid INTEGER REFERENCES RUNS (runsid)," +
@@ -57,7 +55,7 @@ public class SQLiteLogger extends Logger {
 			"runsid INTEGER PRIMARY KEY REFERENCES RUNS (runsid)," +
 			"exitcode INTEGER," +
 			"exception TEXT)";
-	
+
 
 	private static String runsInsert = "INSERT INTO RUNS " +
 			"(runsid, programname, args, whitelist, blacklist, username, timestamp) " +
@@ -72,12 +70,12 @@ public class SQLiteLogger extends Logger {
 			"VALUES (?, ?, ?, ?, ?)";
 
 	private static String  varValuesInsert = "INSERT INTO VARVALUES " +
-			"(runsid, varvaluesid, varsid, linesid, value, type) " +
-			"VALUES (?, ?, ?, ?, ?, ?)";
+			"(runsid, varvaluesid, varsid, linesid, value) " +
+			"VALUES (?, ?, ?, ?, ?)";
 
 	private static String varDeathInsert = "INSERT INTO VARDEATH " +
-			"(runsid, vardeathid, varsid, linesid)" +
-			"VALUES (?, ?, ?, ?)";
+			"(runsid, varsid, linesid)" +
+			"VALUES (?, ?, ?)";
 
 	private static String varUsedInsert = "INSERT INTO VARUSED " +
 			"(runsid, linesid, varsid) " +
@@ -89,15 +87,15 @@ public class SQLiteLogger extends Logger {
 
 	private HashMap<String,Integer> availableVars;
 	private int varCounter;
-	
+
 	private Connection conn;
 	private int runId;
-	
+
 	public SQLiteLogger() {
 		// initialize db, make sure we exist
-		
+
 		availableVars = new HashMap<String, Integer>();
-		
+
 		try {
 			Class.forName("org.sqlite.JDBC");
 			conn = DriverManager.getConnection("jdbc:sqlite:sorbet_out.db");
@@ -109,7 +107,7 @@ public class SQLiteLogger extends Logger {
 			stmt.addBatch(createVarValues);
 			stmt.addBatch(createVars);
 			stmt.executeBatch();
-			
+
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -119,9 +117,9 @@ public class SQLiteLogger extends Logger {
 		}
 
 	}
-	
 
-	@Override 
+
+	@Override
 	public void logProgramStart(String programName, String args, String whitelist, String blacklist) {
 		try {
 			PreparedStatement prep = conn.prepareStatement(runsInsert);
@@ -129,22 +127,22 @@ public class SQLiteLogger extends Logger {
 			prep.setString(1, args);
 			prep.setString(2, whitelist);
 			prep.setString(3, blacklist);
-			
+
 			prep.execute();
-			
+
 			Statement stat = conn.createStatement();
 			ResultSet rs = stat.executeQuery("SELECT runid FROM RUNS ORDER DESC LIMIT 1");
-			
+
 			runId = rs.getInt("runid");
 			line = 0;
 
 			rs.close();
-			
+
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 	}
 
 	@Override
@@ -156,11 +154,11 @@ public class SQLiteLogger extends Logger {
 			prep.setInt(2, line);
 			prep.setString(3, name);
 			prep.setString(4, type);
-			
+
 			prep.execute();
-			
+
 			availableVars.put(name, varCounter);
-			
+
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -169,17 +167,53 @@ public class SQLiteLogger extends Logger {
 
 	@Override
 	public void logVarChanged(String var, String value) {
+		try {
+			PreparedStatement prep = conn.prepareStatement(varValuesInsert);
+			prep.setInt(0, runId);
+			prep.setInt(1, availableVars.get(var));
+			prep.setInt(2, line);
+			prep.setString(3, value);
 
+			prep.execute();
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 	@Override
 	public void logVarDeath(String var) {
+		try {
+			PreparedStatement prep = conn.prepareStatement(varDeathInsert);
+			prep.setInt(0, runId);
+			prep.setInt(1, availableVars.get(var));
+			prep.setInt(2, line);
 
+			prep.execute();
+
+			availableVars.remove(var);
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 	@Override
 	public void logVarUsed(String var) {
+		try {
+			PreparedStatement prep = conn.prepareStatement(varUsedInsert);
+			prep.setInt(0, runId);
+			prep.setInt(1, availableVars.get(var));
+			prep.setInt(2, line);
 
+			prep.execute();
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 	@Override
@@ -189,9 +223,9 @@ public class SQLiteLogger extends Logger {
 			prep.setInt(0, runId);
 			prep.setInt(1, exitCode);
 			prep.setString(2, exception);
-			
+
 			prep.execute();
-			
+
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -205,9 +239,9 @@ public class SQLiteLogger extends Logger {
 			prep.setInt(0, runId);
 			prep.setString(1, filePath);
 			prep.setInt(2, lineNum);
-			
+
 			prep.execute();
-			
+
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
