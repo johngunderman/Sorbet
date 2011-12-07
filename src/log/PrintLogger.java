5ee7282ef5e915b1dd537d49dfda4953bf51624d
Jacob Snyder <jrs213@case.edu>
2011-12-06 21:35:57 -0500

    Better PrintLogger output formatting

--- src/log/PrintLogger.java
+++ src/log/PrintLogger.java
@@ -14,37 +14,37 @@ public class PrintLogger extends Logger {
 	@Override
 	public void logVarCreated(String name, String type) {
 		// TODO Auto-generated method stub
-		System.out.println("Variable created: [" + type + "] " + name );
+		System.out.println("\tVariable created: [" + type + "] " + name );
 	}
 
 	@Override
 	public void logVarChanged(String var, String value) {
 		// TODO Auto-generated method stub
-		System.out.println(">>> " + var + " = " + value);
+		System.out.println("\t>>> " + var + " = " + value);
 	}
 
 	@Override
 	public void logVarDeath(String var) {
 		// TODO Auto-generated method stub
-		System.out.println("Var Death: " + var);
+		System.out.println("\tVariable death: " + var);
 	}
 
 	@Override
 	public void logVarUsed(String var) {
 		// TODO Auto-generated method stub
-		System.out.println("Var Used: " + var);
+		System.out.println("\tVariable used: " + var);
 	}
 
 	@Override
 	public void logProgramExit(int exitCode, String exception) {
 		// TODO Auto-generated method stub
-		System.out.println("Program Exited with code " + exitCode + " [" + exception + "]");
+		System.out.println("Program exited with code " + exitCode + " [" + exception + "]");
 	}
 
 	@Override
 	public void logLines(String filePath, int lineNum) {
 		// TODO Auto-generated method stub
-		System.out.println("Executing: " + filePath + ":" + lineNum);
+		System.out.println("\nLine executed: " + filePath + ":" + lineNum);
 	}
 
 
